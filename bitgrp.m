d59186bbf9e0c9c00661b4683efcd0ba0e9865eb
pszkotak <piotrszkotak92@gmail.com>
2014-05-25 11:23:46 +0200

    working bitgrp.m!

--- bitgrp.m
+++ bitgrp.m
@@ -3,24 +3,13 @@ len=length(bitstr);
 bitspersymbol=log2(m);
 i=1;
 j=1;
-%while i < (len/(bitspersymbol+1))
-while i< 100
-for k = (i*bitspersymbol) : (2*i*bitspersymbol)
-   symb(j, i)=bitstr(k, : )*2^(i-1);
-    i=i+1;
+k=1;
+while k<(len+1)
+    while i<(bitspersymbol+1)
+        symb(j,i)=bitstr(k);
+        k=k+1;
+        i=i+1;
     end
-    k=k+1;
-end
-k
+i=1;
 j=j+1;
 end
-
-%Y = typecast(X, type)
-
-
-
-function symb = bitgrp(bitstr, m)
-len=length(bitstr)
-bps=log2(m)
-
-while i < 
