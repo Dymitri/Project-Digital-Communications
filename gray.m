87840b29fde7274b013ca3a09aa5a20af0ecc7ed
pszkotak <piotrszkotak92@gmail.com>
2014-05-27 13:55:05 +0200

    working gray.m constellation generator for M order QAM modulation. added map2gray function.

--- gray.m
+++ gray.m
@@ -1,16 +1,25 @@
-function r = gray(num)
-    
+function [map] = gray(M)
+
+k = log2(M); % number of bits in each constellation
 
 
-o=bitxor(bitshift(num,1),num);
+alphaRe = [-(2*sqrt(M)/2-1):2:-1 1:2:2*sqrt(M)/2-1];
+alphaIm = [-(2*sqrt(M)/2-1):2:-1 1:2:2*sqrt(M)/2-1];
+% input - decimal equivalent of all combinations with b0b1b2b3
 
-% return (num >> 1) ^ num;
-i=0;
-while 2^i < num
-    i=i+1;
+ip = [0:(M-1)];
+ipBin = dec2bin(ip.'); % decimal to binary
+% taking b0b1 for real
+ipDecRe = bin2dec(ipBin(:,[1:k/2]));
+ipGrayDecRe = bitxor(ipDecRe,floor(ipDecRe/2));
+% taking b2b3 for imaginary
+ipDecIm = bin2dec(ipBin(:,[k/2+1:k]));
+ipGrayDecIm = bitxor(ipDecIm,floor(ipDecIm/2))
+% mapping the Gray coded symbols into constellation
+modRe = alphaRe(ipGrayDecRe+1);
+modIm = alphaIm(ipGrayDecIm+1);
+% complex constellation
+mod = modRe + j*modIm;
+
+map=[ip; modRe; modIm]';
 end
-
-r=bitand(o, 3);
-i
-num
-end 
---
+++ map2gray.m
@@ -0,0 +1,8 @@
+function [ outvec ] = map2gray( invec )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+
+end
+
