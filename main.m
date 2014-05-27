6b1847fd6f7955eb2e28a2fc696a5b0c6c6af64a
pszkotak <piotrszkotak92@gmail.com>
2014-05-27 21:48:15 +0200

    first attempts of filtration

--- main.m
+++ main.m
@@ -9,7 +9,7 @@
 % kanal = 2;
 % BR = 64000;
 
-%nsamp = 4; % Oversampling rate
+nsamp = 4; % Oversampling rate
 %Tb = 1/BR;     %perioda 1 bit
 %tau = 1.0e-6;
 %shift = round(tau/Tb);
@@ -21,7 +21,7 @@ stream=RandBitStream(n);
 
 %mapping
 [ map, complex_constell ]=gray(M); %constellation
-scatterplot(complex_constell);
+
 
 grouped_bits=bitgrp(stream, M);
 symbols=binary2dec(grouped_bits);
@@ -30,6 +30,26 @@ mapped=map2gray(symbols, map);
 %splitting
 
 [I, Q]=split_stream(mapped);
+
+
+
+% Filter Definition
+% Define filter-related parameters.
+filtorder = 40; % Filter order
+delay = filtorder/(nsamp*2); % Group delay (# of input samples)
+ rolloff = 0.25; % Rolloff factor of filter
+
+% Create a square root raised cosine filter.
+rrcfilter = rcosine(1,nsamp,'fir/sqrt',rolloff,delay);
+
+% Transmitted Signal
+% Upsample and apply square root raised cosine filter.
+ytx = rcosflt(mapped,1,nsamp,'filter',rrcfilter);
+fig=scatterplot(complex_constell);
+hold on;
+pause;
+scatterplot(ytx(:,2:3), 1, 0,'g.',fig);
+
 
 
 
