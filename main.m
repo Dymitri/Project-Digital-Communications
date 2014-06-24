7bdd1e23afa779c88dbe63c11ec4142a2c9e358c
pszkotak <piotrszkotak92@gmail.com>
2014-06-24 12:48:58 +0200

    added documentation

--- main.m
+++ main.m
@@ -16,7 +16,7 @@ clc; clear; close all;
 
   
  M = 16; % order of the modulation
- n = 21; % number of bits in bitstream
+ n = 15; % number of bits in bitstream
  k=log2(M); % bits per symbol
  EbNo = 20; % to calculate snr
  fc=40; %frequency of the carrier
